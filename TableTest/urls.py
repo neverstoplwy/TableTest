21d0d79a3d2eec3c601ba1eec8dfc7a3a81538c0
lwy-notebook <liuwy@huayitech.com>
2013-10-12 16:24:55 +0800

    为学习使用web的图表开发的临时工程。 包括了editable和js的学习验证。 js和html的学习是在w3c的在线网站上进行的。

---
+++ TableTest/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+from tt.views import index
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'TableTest.views.home', name='home'),
+    # url(r'^TableTest/', include('TableTest.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    ('^$', index),
+)
